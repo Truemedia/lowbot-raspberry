564a3be9c395a9ebc088c2f9447058f9dfc54df9
Wade Penistone <wadespackages@yahoo.com>
2019-03-17 21:04:19 +0000

    Fixed compat

--- tests/stream.js
+++ tests/stream.js
@@ -1,24 +1,16 @@
-import { StreamCamera, Codec } from "pi-camera-connect";
-import * as fs from "fs";
+const { StreamCamera, Codec } = require("pi-camera-connect");
 
-// Capture 5 seconds of H264 video and save to disk
-const runApp = async () => {
+const streamCamera = new StreamCamera({
+    codec: Codec.H264
+});
 
-    const streamCamera = new StreamCamera({
-        codec: Codec.H264
-    });
+const writeStream = fs.createWriteStream("video-stream.h264");
 
-    const videoStream = streamCamera.createStream();
+const videoStream = streamCamera.createStream();
 
-    const writeStream = fs.createWriteStream("video-stream.h264");
+videoStream.pipe(writeStream);
 
-    videoStream.pipe(writeStream);
+streamCamera.startCapture().then(() => {
 
-    await streamCamera.startCapture();
-
-    await new Promise(resolve => setTimeout(() => resolve(), 5000));
-
-    await streamCamera.stopCapture();
-};
-
-runApp();
+    setTimeout(() => streamCamera.stopCapture(), 5000);
+});
