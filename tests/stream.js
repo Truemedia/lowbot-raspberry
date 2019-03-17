c1aa75bdfa5896f2c7532a03e7e2fbefb07b03a1
Wade Penistone <wadespackages@yahoo.com>
2019-03-17 20:58:13 +0000

    another capture test

--- tests/stream.js
+++ tests/stream.js
@@ -1,7 +1,24 @@
-var raspivid = require('raspivid');
-var fs = require('fs');
+import { StreamCamera, Codec } from "pi-camera-connect";
+import * as fs from "fs";
 
-var file = fs.createWriteStream(__dirname + '/video.h264');
-var video = raspivid();
+// Capture 5 seconds of H264 video and save to disk
+const runApp = async () => {
 
-video.pipe(file);
+    const streamCamera = new StreamCamera({
+        codec: Codec.H264
+    });
+
+    const videoStream = streamCamera.createStream();
+
+    const writeStream = fs.createWriteStream("video-stream.h264");
+
+    videoStream.pipe(writeStream);
+
+    await streamCamera.startCapture();
+
+    await new Promise(resolve => setTimeout(() => resolve(), 5000));
+
+    await streamCamera.stopCapture();
+};
+
+runApp();
